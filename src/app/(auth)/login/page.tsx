357c821765b4004a91f1b3e3425298652216815f
ddrag23 <kurodyami@gmail.com>
2024-07-25 11:56:39 +0700

    improvement fontsize

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,20 +4,21 @@ import authMobileImage from '../../../../public/assets/images/auth-mobile.jpg'
 import Image from "next/image";
 import Link from "next/link";
 import { useMediaQuery } from "@mantine/hooks";
+import { CSSProperties } from "react";
 export default function Page() {
     const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
-
+    const hrefStyle: CSSProperties = { textDecoration: 'none', textAlign: 'right', color: "var(--mantine-color-blue-5)" };
     return <Flex direction={'column'} gap={2} style={{ width: isSmallDevice ? '100%' : '70%' }}>
         {isSmallDevice && <Flex justify={'center'}>
             <Image src={authMobileImage} alt="mobile image" width={300} height={300} />
         </Flex>}
         <Text size="xl" fw={'bold'}>Login</Text>
-        <Text>Create account?</Text>
+        <Text>Create account? {<Link href={'#'} style={hrefStyle}>Register</Link>}</Text>
         <Space h='xl'></Space>
         <Flex direction={'column'} gap={5}>
             <TextInput label="Email" />
             <PasswordInput label="Password" />
-            <Link href={'#'} style={{ textDecoration: 'none', textAlign: 'right', color: "var(--mantine-color-blue-5)" }}>Forgot Password</Link>
+            <Link href={'#'} style={{ ...hrefStyle, fontSize: 12 }}>Forgot Password</Link>
             <Button style={{ marginTop: 10 }}>Login</Button>
         </Flex>
     </Flex>
